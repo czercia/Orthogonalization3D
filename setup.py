c9368a74c53e93b1d2c58da9d8c3828371666667
Marta <m.sroczynska@student.uw.edu.pl>
2016-07-04 20:07:22 +0200

    initial commit setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension("integrals3D",
+                  ["integrals3D.pyx"],
+                  ),
+    ])
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension("orthogonalization",
+                  ["orthogonalization.pyx"],
+                  ),
+    ])
