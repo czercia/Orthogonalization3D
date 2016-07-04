0f7f43073b05f716f4fc88583493c92382a9be02
Marta <m.sroczynska@student.uw.edu.pl>
2016-07-04 20:22:00 +0200

    remove argument r4 from H

--- orthogonalization.pyx
+++ orthogonalization.pyx
@@ -13,15 +13,10 @@ def calculate_matrix(np.ndarray mpp, np.ndarray mpm, np.ndarray mmp, np.ndarray 
     result = np.concatenate((m1, m2), axis = 0)
     return result
 
-def calculate_H(np.ndarray S, np.ndarray A, np.ndarray R, double d, np.ndarray ni, int r4=0):
-    cdef  np.ndarray[np.float64_t, ndim = 2] H, E, EPP, EPM, E_temp,dd
+def calculate_H(np.ndarray S, np.ndarray A, np.ndarray R, double d, np.ndarray ni):
+    cdef  np.ndarray[np.float64_t, ndim = 2] H, E, EPP, EPM, E_temp
     EPP = np.zeros((ni.shape[0], ni.shape[0]))
-    if r4 == 0:
-        E_temp = np.tile(ni- 0.5 *d *d  , (ni.shape[0], 1))
-    else:
-        E_temp = np.tile((ni + 0.5)- 0.5 *d *d  , (ni.shape[0], 1))
-    # print "E_temp"
-    # print E_temp
+    E_temp = np.tile(ni- 0.5 *d *d  , (ni.shape[0], 1))
     cdef Py_ssize_t i, j
     for i in range(ni.shape[0]):
         for j in range(ni.shape[0]):
@@ -32,15 +27,10 @@ def calculate_H(np.ndarray S, np.ndarray A, np.ndarray R, double d, np.ndarray n
 
     EPP = EPP + np.transpose(EPP)
     EPM = E_temp
-    # print "EPP"
-    # print EPP
     EMP = np.transpose(EPP)
     e1 = np.concatenate((EPP, EPM), axis=1)
     e2 = np.concatenate((EPM.T, EPP), axis=1)
     E = np.concatenate((e1, e2), axis = 0)
-
-    # print ("E (d=" + str(d) +"):", E)
-    # dd = -0.5 * d * d * np.ones((S.shape[0], S.shape[1]))
     H =  A +R+ np.multiply(E , S)
     return H,E
 
